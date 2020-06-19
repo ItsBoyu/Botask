453b1ef5540dcbb0d58abf89876e56ee1d9f2e7d
Boyu <rider945@outlook.com>
2020-06-19 13:45:57 +0800

    Step 7: modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  root "tasks#index"
+
+  resources :tasks
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
