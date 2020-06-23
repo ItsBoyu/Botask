93e3bbc9c77a437b922b6dc8c5a4bb42eaa84904
Boyu <rider945@outlook.com>
2020-06-23 10:14:16 +0800

    Step 9: modify features_apec

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
-RSpec.feature "Tasks", type: :feature, driver: :selenium_chrome, js: true do
+RSpec.feature 'Tasks', type: :feature, driver: :selenium_chrome, js: true do
   let!(:task) { Task.create }
 
-  context "Task Create" do
-    scenario "User creates a new task" do
+  context 'Task Create' do
+    scenario 'User creates a new task' do
       visit root_path
-      click_link "add"
-      fill_in "task_title", with: "新增任務名稱"
-      fill_in "task_content", with: "新增任務內容"
-      click_button "Create Task"
+      click_link I18n.t('add')
+      fill_in 'task_title', with: '新增任務名稱'
+      fill_in 'task_content', with: '新增任務內容'
+      click_button I18n.t('task.submit', action: I18n.t('Create'))
 
       expect(current_url) == root_path
-      expect(page).to have_text("You create a task succesfully!")
-      expect(page).to have_text("新增任務名稱")
-      expect(page).to have_text("新增任務內容")
+      expect(page).to have_text(I18n.t('task.created'))
+      expect(page).to have_text('新增任務名稱')
+      expect(page).to have_text('新增任務內容')
     end
   end
 
-  context "Task Read" do
-    scenario "User views the task" do
+  context 'Task Read' do
+    scenario 'User views the task' do
       visit root_path
       click_link "/tasks/#{task.id}"
 
@@ -28,27 +28,27 @@ RSpec.feature "Tasks", type: :feature, driver: :selenium_chrome, js: true do
     end
   end
 
-  context "Task Update" do
-    scenario "User updates the task" do
+  context 'Task Update' do
+    scenario 'User updates the task' do
       visit root_path
-      click_link "edit", { href: "/tasks/#{task.id}/edit" }
-      fill_in "task_title", with: "修改任務名稱"
-      fill_in "task_content", with: "修改任務內容"
-      click_button "Update Task"
+      click_link I18n.t('edit'), { href: "/tasks/#{task.id}/edit" }
+      fill_in 'task_title', with: '修改任務名稱'
+      fill_in 'task_content', with: '修改任務內容'
+      click_button I18n.t('task.submit', action: I18n.t('Update'))
 
       expect(current_url) == root_path
-      expect(page).to have_text("You update the task succesfully!")
-      expect(page).to have_text("修改任務名稱")
-      expect(page).to have_text("修改任務內容")
+      expect(page).to have_text(I18n.t('task.updated'))
+      expect(page).to have_text('修改任務名稱')
+      expect(page).to have_text('修改任務內容')
     end
   end
 
-  context "Task Destroy" do
-    scenario "User deletes the task" do
+  context 'Task Destroy' do
+    scenario 'User deletes the task' do
       visit root_path
-      click_link "delete", { href: "/tasks/#{task.id}" }
+      click_link I18n.t('delete'), { href: "/tasks/#{task.id}" }
 
-      expect(page).to have_text("You delete the task succesfully!")
+      expect(page).to have_text(I18n.t('task.deleted'))
       expect(Task.find_by(id: "#{task.id}")).to be nil
     end
   end
