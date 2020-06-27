bd14aa8b347e8f497d2ef7466e0b8e998eefb6e0
Boyu <rider945@outlook.com>
2020-06-27 23:50:16 +0800

    Step 15: modify enum and add tmp helper

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module TasksHelper
+  def enum_collection(col, act)
+    if act == 'db'
+      Task.send(col.pluralize).keys.map { |key| [t_enum(col, key), Task.send(col.pluralize)[key]] }
+    else
+      Task.send(col.pluralize).keys.map { |key| [t_enum(col, key), key] }
+    end
+  end
+
+  def t_enum(col, key)
+    I18n.t("task.#{col.pluralize}.#{key}")
+  end
 end
