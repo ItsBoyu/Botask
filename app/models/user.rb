ca0b1aae2ff7d6cb30486101660dd37e86c87eb5
Boyu <rider945@outlook.com>
2020-07-06 16:14:25 +0800

    feature 20: add validator to model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@
 
 class User < ApplicationRecord
   has_many :tasks
+
+  validates :email, presence: true,
+                    uniqueness: true
+
+  validates :password, presence: true, 
+                       confirmation: true, 
+                       length: { minimum: 6 }
+
+  validates :password_confirmation, presence: true
+  validates :name, presence: true
 end
