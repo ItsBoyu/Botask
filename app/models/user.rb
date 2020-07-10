d5b3041b5a603813175fdd8b9c8e7d556d3bb9a9
Boyu <rider945@outlook.com>
2020-07-10 18:08:28 +0800

    feature 21: set user's tasks dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 
   validates :email, presence: true,
                     uniqueness: true
